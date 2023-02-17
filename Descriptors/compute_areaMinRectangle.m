bd416415bd4de1b39400a5bce57f557a6919d77d
Adri268 <a.colombo261@campus.unimib>
2023-02-17 09:57:47 +0100

    added comments on descriptors code

--- Descriptors/compute_areaMinRectangle.m
+++ Descriptors/compute_areaMinRectangle.m
@@ -1,10 +1,11 @@
 function x = compute_areaMinRectangle(bwImage)
-    [m, n] = size(bwImage);
     stats = regionprops(bwImage, "MajorAxisLength");
     majorax = stats.MajorAxisLength;
     stats = regionprops(bwImage, "MinorAxisLength");
     minax = stats.MinorAxisLength;
     area = sum(sum(bwImage));
+    %l'area del rettangolo minimo è il prodotto tra l'asse minore e l'asse
+    %maggiore 
     area_Rect = majorax * minax;
     x = area/area_Rect;
 end
